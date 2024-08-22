707385f7f5a0b459e6006a06c754a3a3751af1ee
jerry <jerry@capybaralabs.xyz>
2024-08-22 20:16:54 +0800

    update: sdk sepolia contract

--- dist/sdk.esm.js
+++ dist/sdk.esm.js
@@ -45,7 +45,8 @@ const factory_addresses721 = chainId => {
     [56]: '',
     [4]: '',
     [5]: '',
-    [11155111]: '0xc5af92f871Aac038e336c9a04bbAFE45C81916f3',
+    // [11155111]: '0xc5af92f871Aac038e336c9a04bbAFE45C81916f3',
+    [11155111]: '0xc69bdaA5f4D00C44b3D96cBb4278ec96a2438b54',
     [137]: '',
     [128123]: '0xE3E1bc899fa11908A39f2b8d3E62EA0650229c0C'
   }
@@ -57,7 +58,8 @@ const factory_addresses = chainId => {
     [56]: '0xa1bf45AF7cDe8c105054611383E8ae3dA65615a3',
     [4]: '0xB4AA7ce0558A8EbfC943928d6F3E5ceCc1650a46',
     [5]: '0x075333bF761555D28D78E40232ABdC548083C3D7',
-    [11155111]: '0x83b115AdC58A951B77cD249b41977B2Fd1f13772',
+    // [11155111]: '0x83b115AdC58A951B77cD249b41977B2Fd1f13772',
+    [11155111]: '0x3C6cBe6fd574d7902EA791f897F5efcA749ca71E',
     [137]: '0x590184D7c808c1f68304dEbDCeABec039226CBe5'
   }
   return addresses[chainId] ? addresses[chainId] : addresses[5]
@@ -71,7 +73,8 @@ const init_hash_721 = chainId => {
     [56]: '',
     [4]: '',
     [5]: '',
-    [11155111]: '0x92b5b00a5369e3cbce91ff0263d855551da6acef450f1b715128f900dfcce24d',
+    // [11155111]: '0x92b5b00a5369e3cbce91ff0263d855551da6acef450f1b715128f900dfcce24d',
+    [11155111]: '0x1d8c22081a83572671903b6eb67d31e7f48fc44e240ae5422ef85558ccc0c0cb',
     [137]: '',
     [128123]: '0xc4bf1712e53e05476e2ef9da9206ce842bb68d2dd45e881fd44a58a75d7b11a6'
   }
@@ -83,7 +86,8 @@ const init_hash = chainId => {
     [56]: '0x9ec8c8ff3b18dfb14fac50eaf8f1f0222c424b82ab776a7b394271de4c1a8e9f',
     [4]: '0x7842a3ea267789f9cac67fced95cf3f35c1f5f518317922ae0e6889f2e088f0b',
     [5]: '0x340b5d80515b108122de5e560de62961080d006845634656ff84db6d94c7d069',
-    [11155111]: '0x340b5d80515b108122de5e560de62961080d006845634656ff84db6d94c7d069',
+    // [11155111]: '0x340b5d80515b108122de5e560de62961080d006845634656ff84db6d94c7d069',
+    [11155111]: '0x4417895975b26df01a49cace92985a2d51e6b2d9d7cdc6d1e01a6bc4fe4572d1',
     [137]: '0x91a521d3200632f6f18c2d161b34446d17ee5a8126366b0e8c30a668837f4a6d'
   }
   return hash[chainId] ? hash[chainId] : hash[5]
